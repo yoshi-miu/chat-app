c65db8caba78d32f0862f9cd2b0514d27ed5ae0a
yoshi-miu <68325686+yoshi-miu@users.noreply.github.com>
2020-07-24 11:01:50 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
-  validates :content, presence: true
-
   belongs_to :room
   belongs_to :user
+  has_one_attached :image
+
+  validates :content, presence: true
 end
